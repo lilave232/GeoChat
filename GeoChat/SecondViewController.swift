0dd88b7e3e71b43eccb20b0de32019f14746ac1a
lilave232 <averypozzobon@gmail.com>
2019-01-26 22:23:56 -0500

    Initial Commit

---
+++ GeoChat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GeoChat
+//
+//  Created by Avery Pozzobon on 2019-01-26.
+//  Copyright © 2019 Avery Pozzobon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
